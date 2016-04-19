c39a14e637957e6e867bc73a5279f0ff86ca8f2b
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-04-20 00:56:47 +0530

    Added button and text field in MainActivity.java and set onclicklistener to button

--- app/src/main/java/com/mystoryapp/sangwan/interactivestroy_app/MainActivity.java
+++ app/src/main/java/com/mystoryapp/sangwan/interactivestroy_app/MainActivity.java
@@ -2,12 +2,28 @@ package com.mystoryapp.sangwan.interactivestroy_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNameField = (EditText)findViewById(R.id.editText);
+        mButton = (Button)findViewById(R.id.startButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = mNameField.getText().toString();
+                Toast.makeText(MainActivity.this,text,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
