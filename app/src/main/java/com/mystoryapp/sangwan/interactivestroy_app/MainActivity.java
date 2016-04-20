57d13e366aaae5915cea2b0f161fcfd5a8f266b2
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-04-20 16:45:35 +0530

    Removed Toast message and Added an intent to start StoryActivity in MainAcitivty

--- app/src/main/java/com/mystoryapp/sangwan/interactivestroy_app/MainActivity.java
+++ app/src/main/java/com/mystoryapp/sangwan/interactivestroy_app/MainActivity.java
@@ -1,11 +1,11 @@
 package com.mystoryapp.sangwan.interactivestroy_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +22,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String text = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this,text,Toast.LENGTH_SHORT).show();
+                startStory(text);
             }
         });
+    }
+    private void startStory(String name){
+        Intent in = new Intent(this,StoryActivity.class);
+        in.putExtra("name",name);
+        startActivity(in);
     }
 }
